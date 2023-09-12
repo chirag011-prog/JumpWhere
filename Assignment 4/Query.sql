ae10357b1f99e414a43e162f5a06e9a2e009fafd
Chirag <99155573+chirag011-prog@users.noreply.github.com>
2023-09-12 23:08:14 +0530

    Update Query.sql

--- Assignment 4/Query.sql
+++ Assignment 4/Query.sql
@@ -48,3 +48,62 @@ alter table emp modify Ename varchar(250);
 --13. Write query to get current datetime
 select current_timestamp();
 
+--14. Write a statement to create STUDENT table, with related 5 columns
+create table student(student_id int primary key,
+first_name varchar(20) not null,
+last_name varchar(20),
+dob date,
+gender char(1),
+branch varchar(20)
+);
+
+--15.  Write a query to fetch number of employees in who is getting salary more than 10000
+select count(Ename) from emp where Sal>10000;
+
+--16. Write a query to fetch minimum salary, maximum salary and average salary from emp table.
+select min(Sal), max(Sal), avg(Sal) from emp;
+
+--17. Write a query to fetch number of employees in each location
+select count(e.Ename), d.Loc from department d
+left join emp e on  d.DeptNo = e.DeptNo group by d.Loc;
+
+--18. Write a query to display emplyee names in descending order
+select Ename from emp order by Ename desc;
+
+--19. Write a statement to create a new table(EMP_BKP) from the existing EMP table 
+create table emp_bkp as select * from emp;
+
+--20. Write a query to fetch first 3 characters from employee name appended with salary.
+select concat(substring(Ename,1,3),Sal) from emp;
+
+--21. Get the details of the employees whose name starts with S
+select * from emp where Ename like 'S%';
+
+--22. Get the details of the employees who works in Bangalore location
+select e.* from emp e join department d on e.DeptNo = d.DeptNo
+where d.Loc="Bangalore";
+
+--23.  Write the query to get the employee details whose name started within  any letter between  A and K
+select * from emp where Ename like '[A-K]%';
+
+--24. Write a query in SQL to display the employees whose manager name is Stefen 
+select e1.* from emp e1
+join emp e2 on e1.mgr=e2.EmpNo where e2.Ename="Stefen";
+
+--25. Write a query in SQL to list the name of the managers who is having maximum number of employees working under him
+select mgr
+from(
+    select mgr, count(*) as ecount
+    from emp
+    group by mgr
+    order by ecount desc
+    limit 1
+) as MaxEmployees
+join emp e on MaxEmployees.mgr = e.EmpNo;
+
+--27. Write a query to list all details of all the managers
+select * from emp where EmpNo in(select distinct mgr from 
+emp where mgr is not null);
+
+
+
